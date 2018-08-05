86c06181c49d040956d26fc487cd2978a696e152
annaboyatyuk <annaboyatyuk@gmail.com>
2018-08-04 23:20:50 -0700

    readme

--- src/components/search/searchForm.js
+++ src/components/search/searchForm.js
@@ -35,7 +35,9 @@ export default class SearchForm extends React.Component {
   render() {
     return (
       <form onSubmit={this.handleSubmit} className={this.props.failure ? 'error' : null}>
+        <p>Search topics on Reddit</p>
         <input onChange={this.handleSearch} placeholder='search reddit'/>
+        <p>Limit the number of results</p>
         <input onChange={this.handleRange} type='number' min='1' max='100'/>
         <input type="submit"/>
       </form>
